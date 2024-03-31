440901291f04098efd427ef7d330069d72bd8ed9
Dunja999 <119264678+Dunja999@users.noreply.github.com>
2024-03-31 23:07:12 +0200

    Add files via upload

---
+++ src/memory_game/serverThread.java
@@ -0,0 +1,81 @@
+package memory_game;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class serverThread extends Thread{
+	
+	private Socket prviIgrac;
+	private Socket drugiIgrac;
+	
+	public serverThread(Socket prviIgrac, Socket drugiIgrac) {
+		this.prviIgrac = prviIgrac;
+		this.drugiIgrac = drugiIgrac;
+	}
+	
+	public String postaviRandomPozicije() {
+		
+		ArrayList<String> pozicije = new ArrayList<> ();
+		 for(int i = 0; i < 10; i++) {
+			 pozicije.add(i + "");
+			 pozicije.add(i + "");
+		 }
+		 
+		 System.out.println(pozicije);
+		 Collections.shuffle(pozicije);
+		 System.out.println(pozicije);
+		 
+		 return pozicije.toString();
+	}
+	
+	public void run() {
+		try {
+			
+			BufferedReader prviIgracInput = new BufferedReader(new InputStreamReader(prviIgrac.getInputStream()));
+			PrintWriter prviIgracOutput = new PrintWriter(prviIgrac.getOutputStream(), true);
+			String ime = prviIgracInput.readLine();
+			System.out.println("Prvi igrac: " + ime);
+			
+			BufferedReader drugiIgracInput = new BufferedReader(new InputStreamReader(drugiIgrac.getInputStream()));
+			PrintWriter drugiIgracOutput = new PrintWriter(drugiIgrac.getOutputStream(), true);
+			ime = drugiIgracInput.readLine();
+			System.out.println("Drugi igrac: " + ime);
+			
+			String randomPozicije = postaviRandomPozicije();
+			System.out.println(randomPozicije);
+			
+			prviIgracOutput.println("start");
+			prviIgracOutput.println(randomPozicije);
+			
+			drugiIgracOutput.println("start");
+			drugiIgracOutput.println(randomPozicije);
+			
+			drugiIgracOutput.println("pause");
+			prviIgracOutput.println("play");
+			
+			prviIgrac.close();
+			prviIgracInput.close();
+			prviIgracOutput.close();
+			
+			drugiIgrac.close();
+			drugiIgracInput.close();
+			drugiIgracOutput.close();
+			
+			
+	} catch (SocketException e) {
+		
+	} catch (IOException e) {
+		e.printStackTrace();
+	
+	}
+	}
+	
+
+}
