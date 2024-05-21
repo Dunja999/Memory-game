ef7dd4af5dd61fa715d059fddd7f56bdda98b856
Dunja999 <119264678+Dunja999@users.noreply.github.com>
2024-05-21 23:40:54 +0200

    Update Server.java

--- src/memory_game/Server.java
+++ src/memory_game/Server.java
@@ -5,33 +5,34 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	
-	private final static int port = 11111;
-	private ServerSocket serverSocket;
-	
-	   public Server() throws IOException{
-	    serverSocket = new ServerSocket(port);
-		 System.out.println("Server osluskuje na portu: " + port);
-		  execute();
-	   }
-	   
-	   public void execute() {
-		   
-		   while(true) {		
-				try {
-					Socket prviIgrac = serverSocket.accept();
-					Socket drugiIgrac = serverSocket.accept();
-					
-					new serverThread(prviIgrac, drugiIgrac).start();
-					
-				} catch (IOException e) {
-					e.printStackTrace();
-				}			
-			}	
+
+	private final static int port = 8001;
+	private ServerSocket serverSocket = null;
+
+	public Server() throws IOException {
+		serverSocket = new ServerSocket(port);
+		System.out.println("Server osluskuje na portu: " + port);
+		execute();
+	}
+
+	public void execute() {
+
+		while (true) {
+			try {
+				Socket prviIgrac = serverSocket.accept();
+				Socket drugiIgrac = serverSocket.accept();
+
+				new ServerThread(prviIgrac, drugiIgrac).start();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
-	   
-	   }
+	}
 
+	public static void main(String[] args) throws IOException {
+		new Server();
+	}
 
-
+}
