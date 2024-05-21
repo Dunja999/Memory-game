b8d928ffa7a58874305ded8894cbdafcdd037a82
Dunja999 <119264678+Dunja999@users.noreply.github.com>
2024-05-21 23:33:13 +0200

    Update Klijent.java

--- src/memory_game/Klijent.java
+++ src/memory_game/Klijent.java
@@ -9,59 +9,82 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Klijent {
-	private static final int PORT = 11111;
+	private static int PORT = 8001;
 	private String username;
 	private String HOST = "localhost";
 	private Socket socket;
 	private BufferedReader input;
 	private PrintWriter output;
-	private int points;
-	
-	public Klijent() {
+	private gui g;
+	public int bodovi = 0;
+
+	public Klijent(String username) {
+		this.username = username;
+	}
+
+	public Klijent(gui g) {
+		this.g = g;
 		try {
 			socket = new Socket(HOST, PORT);
 			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 			System.out.println("Klijent je povezan!");
-		}
-		catch(IOException e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 			closeResourses();
+
 		}
 	}
-	
+
 	public String run() throws IOException {
 		String start = this.input.readLine();
-		System.out.println(start);
-		if(start.equals("start"))
+		if (start.equals("start"))
 			System.out.println("Igra pocinje!");
 		String niz = input.readLine();
-		System.out.println("Raspored: " + niz);
-		
-		
+
 		return niz.substring(1, niz.length() - 1);
 	}
-	
+
+	public void sendUsername() {
+		output.println(username);
+	}
+
+	public void setUsername(String name) {
+		username = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
 	public void closeResourses() {
 		try {
 			socket.close();
 			input.close();
 			output.close();
-		}
-		catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+    //klijent salje serveru poruku
 	public void posaljiPoruku(String poruka) {
-		output.println(poruka);
+		output.println(poruka);// saljemo poruku serveru
 	}
-	
-	public void posaljiPozicije(int prvaPozicija, int drugaPozicija) {
+    
+	public void posaljiPozicije(int prvaPozicija, int drugaPozicija, String bool) {
 
 		System.out.println(prvaPozicija + ", " + drugaPozicija);
-		output.println(prvaPozicija + "," + drugaPozicija );
-		
+		output.println(prvaPozicija + ", " + drugaPozicija + ", " + bool);
+
 	}
-	
+    //klijent cita sta mu je server poslao
+	public String readString() throws IOException {
+
+		String poruka = input.readLine();
+		System.out.println(poruka);
+		return poruka;
+	}
 
 }
+
